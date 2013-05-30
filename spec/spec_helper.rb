f4db42f49ed7aec873d20f8784160a9709eb8436
Damian Aberbuj <daberbuj@keepcon.com>
2013-05-30 17:01:58 -0300

    Hide debug logs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'git_wrapper'
 include GitWrapper
 include GitWrapper::Results
 
+GitWrapper.logger.level = Logger::Severity::ERROR
+
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
