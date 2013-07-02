38b140b36cf1947ec525361a8e00b9e4a0c38f02
Gabriel Naiman <gnaiman@keepcon.com>
2013-07-02 09:40:20 -0300

    Remove unused code

--- spec/support/matchers/git_status_matchers.rb
+++ spec/support/matchers/git_status_matchers.rb
@@ -1,12 +1,3 @@
-RSpec::Matchers.define :eq_git_status do |expected|
-  match do |actual|
-    (expected[:file_name].nil? || actual.file_name == expected[:file_name]) &&
-      (expected[:original_file_name].nil? || actual.original_file_name == expected[:original_file_name]) &&
-      (expected[:status].nil? || actual.status == expected[:status]) &&
-      (expected[:staged_for_commit].nil? || actual.staged_for_commit == expected[:staged_for_commit])
-  end
-end
-
 RSpec::Matchers.define :be_git_untracked do |expected_file_name|
   match do |actual|
     actual.file_name == expected_file_name &&
